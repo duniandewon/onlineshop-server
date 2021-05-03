f54fa688ebb4efb51d17db10f8b8d695812a5efd
Abolfaz <duniandewon@gmail.com>
2021-05-04 01:10:39 +0700

    Get logged in user route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,26 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+
+import auth from '../middlewares/auth'
 import User from '../models/User'
 
 const router = express.Router()
 
+/**
+ * @route   GET api/auth
+ * @desc    Get logged in user
+ * @access  Private
+ */
+ router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 /**
  * @route   POST api/auth
