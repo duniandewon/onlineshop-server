95be50bf40f0d4e49c2654d30490c4f42f1fb0e4
Abolfaz <duniandewon@gmail.com>
2021-05-06 04:57:44 +0700

    Fix product ref

--- models/Cart.js
+++ models/Cart.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const CartSchema = new mongoose.Schema({
   product: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'products',
+    ref: 'product',
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
@@ -13,11 +13,7 @@ const CartSchema = new mongoose.Schema({
   quantity: {
     type: Number,
     require: true,
-  },
-  subtotal: {
-    type: Number,
-    require: true,
-  },
+  }
 });
 
 export default mongoose.models.cart || mongoose.model('cart', CartSchema);
